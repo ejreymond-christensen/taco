b178bcf6b97f59b4764c46ae82c615287643bc32
Elliot Reymond-Christensen <elliotchristensen@gmail.com>
2018-03-02 13:12:32 -0600

    changed port in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var PORT = process.env.JAWSDB_UL || 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
